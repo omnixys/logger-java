3426e0cb38b121c4abbd52670730b7f11b8e71cb
Caleb-Script <caleb-script@outlook.de>
2026-03-22 11:20:35 +0100

    breaking(init): initial comit

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,12 +56,8 @@ dependencies {
     // Logging
     implementation("org.slf4j:slf4j-api")
 
-
-
-
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.springframework.boot:spring-boot-configuration-processor")
-
 }
 
 tasks.test {
