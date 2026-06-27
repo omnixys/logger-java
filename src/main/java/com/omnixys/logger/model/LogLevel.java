fedb5bb6a36a5545b91bcc2878d206f0ef660ecf
Caleb-Script <caleb-script@outlook.de>
2026-06-27 20:28:02 +0200

    breaking(CD/CI): add action workflows

--- src/main/java/com/omnixys/logger/model/LogLevel.java
+++ src/main/java/com/omnixys/logger/model/LogLevel.java
@@ -2,6 +2,7 @@ package com.omnixys.logger.model;
 
 import lombok.Getter;
 
+
 /**
  * Log level definition.
  */
@@ -34,7 +35,11 @@ public enum LogLevel {
     public static class LogLevelSerializer extends tools.jackson.databind.ser.std.StdSerializer<LogLevel> {
         public LogLevelSerializer() { super(LogLevel.class); }
         @Override
-        public void serialize(LogLevel value, tools.jackson.core.JsonGenerator gen, tools.jackson.databind.SerializerProvider provider) throws java.io.IOException {
+        public void serialize(
+                LogLevel value,
+                tools.jackson.core.JsonGenerator gen,
+                tools.jackson.databind.SerializationContext context
+        ) throws tools.jackson.core.JacksonException {
             gen.writeString(value.value);
         }
     }
@@ -42,7 +47,10 @@ public enum LogLevel {
     public static class LogLevelDeserializer extends tools.jackson.databind.deser.std.StdDeserializer<LogLevel> {
         public LogLevelDeserializer() { super(LogLevel.class); }
         @Override
-        public LogLevel deserialize(tools.jackson.core.JsonParser p, tools.jackson.databind.DeserializationContext ctxt) throws java.io.IOException {
+        public LogLevel deserialize(
+                tools.jackson.core.JsonParser p,
+                tools.jackson.databind.DeserializationContext ctxt
+        ) throws tools.jackson.core.JacksonException {
             return LogLevel.fromValue(p.getValueAsString());
         }
     }
