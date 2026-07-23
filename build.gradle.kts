f2eccef754fb18dfe5501284989c10ea2b8d911e
Caleb-Script <caleb-script@outlook.de>
2026-07-23 14:12:33 +0200

    fix(deps): update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     `maven-publish`
     id("org.springframework.boot") version "4.1.0" apply false
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.github.ben-manes.versions") version "0.52.0"
 }
 
 
